662c2df92243992d1e0b260a3277a0aaadd1aaa4
Mslonoff <matthew.slonoff@gmail.com>
2023-12-21 19:40:22 -0600

    get given owner working

--- app.js
+++ app.js
@@ -54,6 +54,22 @@ app.get('/api/vehicles/:id', (req, res) => { // good to go
     });
 });
 
+app.get('/api/owners/:id', (req, res) => {
+    const { id } = req.params;
+    pool.query('SELECT * FROM owners WHERE id=$1', [id])
+    .then((result) => {
+        if (result.rows.length > 0) {
+            res.status(200).send(result.rows)
+        } else {
+            res.status(404).send('Sorry cannot find owner');
+        }
+    })
+    .catch((error) => {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    });
+});
+
 app.listen(expressPort, () => {
     console.log(`Listening on port ${expressPort}...`);
 });
