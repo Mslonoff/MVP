d20c43a1b59dfae0b268f00dcf911773e6cd5c46
Mslonoff <matthew.slonoff@gmail.com>
2023-12-21 22:46:23 -0600

    added owners to DOM

--- public/index.js
+++ public/index.js
@@ -7,13 +7,13 @@ $.get('/api/vehicles', function(data) {
         console.log(currentVehicle);
 
         let $currentVehicleInfo = $('<span class="currentVehicleInfo"></span>');
-        let $vehicleId = $(`<span class="vehicleId">ID: ${currentVehicle.id}</span>`);
-        let $vehicleColor = $(`<span class="vehicleColor">Color: ${currentVehicle.color}</span>`);
-        let $licensePlate = $(`<span class="licensePlate">License Plate: ${currentVehicle.licenseplate}</span>`);
-        let $vehicleMake = $(`<span class="vehicleMake">Vehicle Make: ${currentVehicle.make}</span>`);
-        let $model = $(`<span class="model">Model: ${currentVehicle.model}</span>`);
-        let $year = $(`<span class="year">Year: ${currentVehicle.modelyear}</span>`);
-        let $ownerId = $(`<span class="ownerId">Owner ID: ${currentVehicle.owner_id}</span>`);
+        let $vehicleId = $(`<span class="vehicleId">ID: ${currentVehicle.id} </span>`);
+        let $vehicleColor = $(`<span class="vehicleColor">Color: ${currentVehicle.color} </span>`);
+        let $licensePlate = $(`<span class="licensePlate">License Plate: ${currentVehicle.licenseplate} </span>`);
+        let $vehicleMake = $(`<span class="vehicleMake">Vehicle Make: ${currentVehicle.make} </span>`);
+        let $model = $(`<span class="model">Model: ${currentVehicle.model} </span>`);
+        let $year = $(`<span class="year">Year: ${currentVehicle.modelyear} </span>`);
+        let $ownerId = $(`<span class="ownerId">Owner ID: ${currentVehicle.owner_id} </span>`);
         
         $currentVehicleInfo.append($vehicleId, $vehicleColor, $licensePlate, $vehicleMake, $model, $year, $ownerId);
         $vehicleData.append($currentVehicleInfo);
@@ -21,7 +21,7 @@ $.get('/api/vehicles', function(data) {
     }
 })
 
-$.get('/api/owners', function(data) {
+$.get('/api/owners/:id', function(data) {
     console.log(data);
     let $ownerData = $('<div id="ownerData"></div>');
     $('#main').append($ownerData);
@@ -30,6 +30,11 @@ $.get('/api/owners', function(data) {
         console.log(currentOwner);
 
         let $currentOwnerInfo = $('<span class="currentOwnerInfo"></span>');
+        let $id = $(`<span class="id">ID: ${currentOwner.id} </span>`);
+        let $firstName = $(`<span class="firstName">First Name: ${currentOwner.firstname} </span>`);
+        let $lastName = $(`<span class="lastName">Last Name: ${currentOwner.lastname} </span>`);
+        $currentOwnerInfo.append($id, $firstName, $lastName);
         $ownerData.append($currentOwnerInfo);
+        $currentOwnerInfo.append(currentOwner);
     }
 })
