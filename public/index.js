61640461b0134e40888a7c403b5663a8e07ab943
Mslonoff <matthew.slonoff@gmail.com>
2023-12-21 20:27:28 -0600

    started appending vehicle info

--- public/index.js
+++ public/index.js
@@ -1,20 +1,28 @@
 $.get('/api/vehicles', function(data) {
     console.log(data);
-    let $vehicleData = $('<span class="vehicleData"></span');
-    $('body').append($vehicleData);
+    let $vehicleData = $('<div id="vehicleData"></div>');
+    $('#main').append($vehicleData);
     for (let i = 0; i < data.length; i++) {
         let currentVehicle = data[i];
         console.log(currentVehicle);
 
         let $currentVehicleInfo = $('<span class="currentVehicleInfo"></span>');
+        let $vehicleId = $(`<span class="vehicleId">'ID: '${currentVehicle.id}</span>`);
+        let $vehicleColor = $(`<span class="vehicleColor">'Color: '${currentVehicle.color}</span>`);
+        let $licensePlate = $(`<span class="licensePlate">'License Plate: '${currentVehicle.licenseplate}</span>`);
+        let $vehicleMake = $(`<span class="vehicleMaker">'Vehicle Make: '${currentVehicle.make}</span>`);
+        let $model = $(`<span class="model">'Model: '${currentVehicle.model}</span>`);
+        let $year = $(`<span class="year">'Year: '${currentVehicle.year}</span>`);
+        $currentVehicleInfo.append($vehicleId, $vehicleColor, $licensePlate, $vehicleMake, $model, $year);
         $vehicleData.append($currentVehicleInfo);
+        $currentVehicleInfo.append(currentVehicle);
     }
 })
 
 $.get('/api/owners', function(data) {
     console.log(data);
-    let $ownerData = $('<span class="ownerData"></span>');
-    $('body').append($ownerData);
+    let $ownerData = $('<div id="ownerData"></div>');
+    $('#main').append($ownerData);
     for (let i = 0; i < data.length; i++) {
         let currentOwner = data[i];
         console.log(currentOwner);
