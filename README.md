b051f2a6279f64d1383ef3627d59452ad4116a73
Mslonoff <matthew.slonoff@gmail.com>
2024-01-27 09:44:50 -0500

    readme updates

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # MVP
+
+## Sub Heading
+
+### Sub Heading
+
+
+**Bold Text**
+
+*Italicized Text*
+
+1. First Item
+2. Second Item
+3. Third Item
+
+- First Item
+- Second Item
+- Third Item
+
+< block quote >
+
+`code`
+
+--- Horizontal Rule
+
+[google] [www.google.com]
+
+[!alt text] (image.jpg)
+
+==very important to hightlight==
+
--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const expressPort = 8005;
 //
 const connectionString = process.env.PG_DATABASE_URL; // hide to prevent hacking? USE this for render
 console.log(connectionString);
-const pool = new Pool ({
+const pool = new Pool({
     connectionString,
 })
 
@@ -27,84 +27,95 @@ app.use(express.static('public'));
 app.use(express.json());
 
 // create CRUD routes
-app.get('/api/vehicles', (req, res) => { // good to go
-    pool
-    .query('SELECT * FROM vehicles')
-    .then((result) => res.status(201).send(result.rows))
-    .catch((error) => {
-        console.error(error);
-        res.status(500).send('Sorry your vehicles not found');
-    });
-});
+// app.get('/api/vehicles', (req, res) => { // good to go - USING .then and .catch promise
+//     pool
+//     .query('SELECT * FROM vehicles')
+//     .then((result) => res.status(201).send(result.rows))
+//     .catch((error) => {
+//         console.error(error);
+//         res.status(500).send('Sorry your vehicles not found');
+//     });
+// });
+
+app.get('/api/vehicles', async (req, res) => { // USING async/await with try/catch promise
+    try {
+        const result = await pool.query('SELECT * FROM vehicles'); // awaiting promise (pool.query) to be resolved
+        res.status(201).send(result.rows);
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+    }
+})
 
 app.get('/api/license', (req, res) => { // trying to get all license plates
     pool
-    .query('SELECT * FROM licensePlates')
-    .then((result) => res.status(201).send(result.rows))
-    .catch((error) => {
-        console.error(error);
-        res.status(500).send('Sorry your vehicles not found');
-    });
+        .query('SELECT * FROM licensePlates')
+        .then((result) => res.status(201).send(result.rows))
+        .catch((error) => {
+            console.error(error);
+            res.status(500).send('Sorry your vehicles not found');
+        });
 });
 
 app.get('/api/owners', (req, res) => { // good to go
     pool
-    .query('SELECT * FROM owners')
-    .then((result) => res.status(201).send(result.rows))
-    .catch((error) => {
-        console.error(error);
-        res.status(500).send('Sorry your owners not found');
-    });
+        .query('SELECT * FROM owners')
+        .then((result) => res.status(201).send(result.rows))
+        .catch((error) => {
+            console.error(error);
+            res.status(500).send('Sorry your owners not found');
+        });
 });
 
 app.get('/api/vehicles/:id', (req, res) => { // good to go
     const { id } = req.params;
     pool.query('SELECT * FROM vehicles WHERE id=$1', [id])
-    .then((result) => {
-        if (result.rows.length > 0) {
-            res.status(200).send(result.rows)
-        } else {
-            res.status(404).send('Sorry cannot find vehicle');
-        }
-    })
-    .catch((error) => {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
-    });
+        .then((result) => {
+            if (result.rows.length > 0) {
+                res.status(200).send(result.rows)
+            } else {
+                res.status(404).send('Sorry cannot find vehicle');
+            }
+        })
+        .catch((error) => {
+            console.error(error);
+            res.status(500).send('Internal Server Error');
+        });
 });
 
 app.get('/api/vehicles/license/:license', (req, res) => { // good to go
     const { license } = req.params;
     pool.query('SELECT * FROM vehicles WHERE licensePlate=$1', [license])
-    .then((result) => {
-        if (result.rows.length > 0) {
-            console.log(result.rows)
-            res.status(200).send(result.rows)
-        } else {
-            res.status(404).send('Sorry cannot find vehicle');
-        }
-    })
-    .catch((error) => {
-        //console.log(result.rows)
-        console.error(error);
-        res.status(500).send('Internal Server Error');
-    });
+        .then((result) => {
+            if (result.rows.length > 0) {
+                console.log(result.rows)
+                res.status(200).send(result.rows)
+            } else {
+                res.status(404).send('Sorry cannot find vehicle');
+            }
+        })
+        .catch((error) => {
+            //console.log(result.rows)
+            console.error(error);
+            res.status(500).send('Internal Server Error');
+        });
 });
 
 app.get('/api/owners/:id', (req, res) => { // good to go
     const { id } = req.params;
     pool.query('SELECT * FROM owners WHERE id=$1', [id])
-    .then((result) => {
-        if (result.rows.length > 0) {
-            res.status(200).send(result.rows)
-        } else {
-            res.status(404).send('Sorry cannot find owner');
-        }
-    })
-    .catch((error) => {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
-    });
+        .then((result) => {
+            if (result.rows.length > 0) {
+                res.status(200).send(result.rows)
+            } else {
+                res.status(404).send('Sorry cannot find owner');
+            }
+        })
+        .catch((error) => {
+            console.error(error);
+            res.status(500).send('Internal Server Error');
+        });
 });
 
 // app.get('/api/owners/ownerid:id', (req, res) => { // good to go
